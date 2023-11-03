a087a269469a583a92f86cae82fe5f0fb3e8697e
Aron Diaz <arondiaz.cpn@gmail.com>
2023-11-02 22:23:34 -0300

    review basics of js

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+// declarado e inicializado
+let lenguage = "PHP";
+
+const framework = "Laravel";
+
+// renombrado
+lenguage = "JAVA";
+
+console.log(lenguage);
+
+let string = "dog";
+string = "cat";
+
+/*undefined - declarado pero no inicializado*/ 
+let car;
+console.log(car);
+
+console.log(string);
+
+const coin = "btc";
+console.log(coin);
+
+let age = prompt("ingrese su edad")
+alert(age + " años")
+
+let valueBtc = 35000
+
+valueBtc += 40000
+
+document.write(valueBtc)
+
+
+numberOne = 10;
+numberTwo = 8;
+
+numberOne--;
+
+console.log(numberOne);
