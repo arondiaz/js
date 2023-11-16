5334895407e3ecd1f09468395133fe49e11bfa6c
Arón Díaz <arondiaz.cpn@gmail.com>
2023-11-16 15:32:12 -0300

    add methods for todoapp

--- mid/todoapp/src/store/todo.store.js
+++ mid/todoapp/src/store/todo.store.js
@@ -16,6 +16,46 @@ const initStore = () => {
   console.log("init store :)");
 };
 
+const loadStore = () => {
+  throw new Error("Not implemented");
+};
+
+const getTodo = (filter = Filters.All) => {
+  throw new Error("Not implemented");
+};
+
+const addTodo = (description) => {
+  throw new Error("Not implemented");
+};
+
+const changeTodo = (todoId) => {
+  throw new Error("Not implemented");
+};
+
+const deleteTodo = (todoId) => {
+  throw new Error("Not implemented");
+};
+
+const deleteCompleted = () => {
+  throw new Error("Not implemented");
+};
+
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error("Not implemented");
+};
+
+const getCurrentFilter = () => {
+  throw new Error("Not implemented");
+};
+
 export default {
   initStore,
+  loadStore,
+  getTodo,
+  addTodo,
+  changeTodo,
+  deleteTodo,
+  deleteCompleted,
+  setFilter,
+  getCurrentFilter,
 };
