de970f2109c06595770f43b67fa9ba3d06af0f73
Arón Díaz <arondiaz.cpn@gmail.com>
2023-11-29 15:02:51 -0300

    add comments on indexedDB

--- mid/indexedDB/app.js
+++ mid/indexedDB/app.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function crmDB() {
   //crear base de datos version 1.0
+  // window es un objeto, indexedDB es una propiedad de window, open el metodo y lo que esta entre () son los parametros.
   let crmDB = window.indexedDB.open("crm", 1);
 
   //si hay un error
@@ -27,14 +28,16 @@ function crmDB() {
   //configuracion de la base de datos, se ejecuta 1 sola vez al crearse la base de datos.
   crmDB.onupgradeneeded = function (e) {
     console.log("Este método solo se ejecuta una vez.");
+    //el resultado del evento que se origina al crearse la base de datos.
     const db = e.target.result;
 
+    //
     const objectStore = db.createObjectStore("crm", {
       keyPath: "crm",
       autoIncrement: true,
     });
 
-    //Definimos las columnas,
+    //Definimos las columnas,((primer parametro = indice), (segundo parametro = propiedad), {si necesitan ser unicos o no})
     objectStore.createIndex("nombre", "nombre", { unique: false });
     objectStore.createIndex("email", "email", { unique: true });
     objectStore.createIndex("telefono", "telefono", { unique: false });
@@ -43,7 +46,9 @@ function crmDB() {
   };
 }
 
+//ingreso de registros
 function crearCliente() {
+  //transaction en base de datos: las transacciones se utilizan para agrupar operaciones de lectura y escritura en una unidad coherente y garantizar la integridad de los datos. ([seleccionamos la DB], "sellecionar modo")
   let transaction = DB.transaction(["crm"], "readwrite");
 
   transaction.oncomplete = function () {
@@ -54,6 +59,7 @@ function crearCliente() {
     console.log("Hubo un error");
   };
 
+  //objectStore: una colección de datos estructurados en forma de objetos.
   const objectStore = transaction.objectStore("crm");
 
   const nuevoCliente = {
@@ -62,6 +68,7 @@ function crearCliente() {
     email: "asask@gmail.com",
   };
 
+  //metodos crud, add, put, post, delete
   const peticion = objectStore.add(nuevoCliente);
   console.log(peticion);
 }
