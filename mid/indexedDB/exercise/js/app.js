c1c69e9f34ad290cafdedc8f42eaeadbbb2f7348
Arón Díaz <arondiaz.cpn@gmail.com>
2023-11-29 18:31:38 -0300

    get clients of indexedDB

--- mid/indexedDB/exercise/js/app.js
+++ mid/indexedDB/exercise/js/app.js
@@ -1,4 +1,4 @@
-// // 
+// //
 // La sintaxis (function () {})(); se conoce comúnmente como una "función autoinvocada" o "IIFE" (Immediately Invoked Function Expression) en inglés.
 
 // Esta técnica se utiliza en JavaScript por varias razones:
@@ -16,11 +16,13 @@
 // Control de Tiempo de Ejecución:
 // Al usar una IIFE, el código dentro de la función se ejecuta inmediatamente cuando el archivo se carga, en lugar de esperar una llamada explícita. Esto puede ser útil para realizar configuraciones iniciales o ejecutar código que debe ejecutarse inmediatamente.
 
-
-
 (function () {
   document.addEventListener("DOMContentLoaded", () => {
     crearDB();
+
+    if (window.indexedDB.open("crm", 1)) {
+      obtenerClientes();
+    }
   });
 
   function crearDB() {
@@ -49,6 +51,53 @@
       objectStore.createIndex("id", "id", { unique: true });
 
       console.log("Hi");
+    };
+  }
+
+  function obtenerClientes() {
+    const abrirConexion = window.indexedDB.open("crm", 1);
+    abrirConexion.onerror = function () {
+      console.log("hubo un error");
+    };
+
+    abrirConexion.onsuccess = function () {
+      DB = abrirConexion.result;
+      console.log(DB);
+
+      const objectStore = DB.transaction("crm").objectStore("crm");
+
+      objectStore.openCursor().onsuccess = function (e) {
+        const cursor = e.target.result;
+
+        if (cursor) {
+          const { name, empresa, email, telefono, id } = cursor.value;
+
+          const listadoClientes = document.querySelector("#listado-clientes");
+
+          listadoClientes.innerHTML += `
+          <tr>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${name} </p>
+              <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+          </td>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+              <p class="text-gray-700">${telefono}</p>
+          </td>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+              <p class="text-gray-600">${empresa}</p>
+          </td>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+              <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+              <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+          </td>
+            </tr>
+          `;
+
+          cursor.continue();
+        } else {
+          console.log("no hay registros");
+        }
+      };
     };
   }
 })();
