c4a0c0fc876ddc9a4021ee5b3c6167e875694cce
Arón Díaz <arondiaz.cpn@gmail.com>
2023-11-30 10:39:19 -0300

    now can update clients

--- mid/indexedDB/exercise/js/editarcliente.js
+++ mid/indexedDB/exercise/js/editarcliente.js
@@ -1,18 +1,25 @@
 (function () {
   let DB;
+  //
+  let idCliente;
 
   const nameInput = document.querySelector("#nombre");
   const emailInput = document.querySelector("#email");
   const empresaInput = document.querySelector("#empresa");
   const telefonoInput = document.querySelector("#telefono");
 
+  const form = document.querySelector("#formulario");
+
   document.addEventListener("DOMContentLoaded", () => {
     conectarDB();
+
+    //actualiza el registro
+    form.addEventListener("submit", actualizarCliente);
 
     //verificar el id de la url
     const parametrosUrl = new URLSearchParams(window.location.search);
 
-    const idCliente = parametrosUrl.get("id");
+    idCliente = parametrosUrl.get("id");
 
     //deberia usar async await
     if (idCliente) {
@@ -21,6 +28,48 @@
       }, 100);
     }
   });
+
+  function actualizarCliente(e) {
+    e.preventDefault();
+    if (
+      nameInput.value === "" ||
+      emailInput.value === "" ||
+      empresaInput.value === "" ||
+      telefonoInput.value === ""
+    ) {
+      imprimirAlerta("Todos los campos deben estar completos", "error");
+
+      return;
+    }
+
+    //actualizar cliente
+
+    const clienteActualizado = {
+      name: nameInput.value,
+      email: emailInput.value,
+      empresa: empresaInput.value,
+      telefono: telefonoInput.value,
+      id: Number(idCliente),
+    };
+
+    console.log(clienteActualizado);
+    const transaction = DB.transaction(["crm"], "readwrite");
+    const objectStore = transaction.objectStore("crm");
+
+    objectStore.put(clienteActualizado);
+
+    transaction.oncomplete = function () {
+      imprimirAlerta("editado completamente");
+
+      setTimeout(() => {
+        window.location.href = "index.html"
+      }, 2000);
+    };
+
+    transaction.onerror = function () {
+      imprimirAlerta("hubo un error", "error");
+    };
+  }
 
   function obtenerCliente(id) {
     const transaction = DB.transaction(["crm"], "readwrite");
--- mid/indexedDB/exercise/js/nuevocliente.js
+++ mid/indexedDB/exercise/js/nuevocliente.js
@@ -8,17 +8,7 @@
     form.addEventListener("submit", validarCliente);
   });
 
-  function conectarDB() {
-    const abrirConexion = window.indexedDB.open("crm", 1);
-
-    abrirConexion.onerror = function () {
-      console.log("hubo un error");
-    };
-
-    abrirConexion.onsuccess = function () {
-      DB = abrirConexion.result;
-    };
-  }
+ 
 
   function validarCliente(e) {
     e.preventDefault();
@@ -64,44 +54,5 @@
     };
   }
 
-  function imprimirAlerta(mensaje, tipo) {
-    const alert = document.querySelector(".alerta");
-
-    if (!alert) {
-      const divMensaje = document.createElement("DIV");
-      divMensaje.classList.add(
-        "px-4",
-        "py-3",
-        "rounded",
-        "max-w-lg",
-        "mx-auto",
-        "mt-6",
-        "text-center",
-        "border",
-        "alerta"
-      );
-
-      if (tipo === "error") {
-        divMensaje.classList.add(
-          "bg-red-100",
-          "border-red-400",
-          "text-red-700"
-        );
-      } else {
-        divMensaje.classList.add(
-          "bg-green-100",
-          "border-green-400",
-          "text-green-700"
-        );
-      }
-
-      divMensaje.textContent = mensaje;
-
-      form.appendChild(divMensaje);
-
-      setTimeout(() => {
-        divMensaje.remove();
-      }, 2500);
-    }
-  }
+  
 })();
