18c608e40d39b0fbd4dd5886f9a7775ebfa75a68
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-01 16:57:28 -0300

    update json fetch

--- mid/fetch-API/js/03-app.js
+++ mid/fetch-API/js/03-app.js
@@ -1,6 +1,41 @@
-const jsonArrayBtn = document.querySelector("#cargarJSONArray");
+// const jsonArrayBtn = document.querySelector("#cargarJSONArray");
 
-jsonArrayBtn.addEventListener("click", llamarArray);
+// jsonArrayBtn.addEventListener("click", llamarArray);
+
+// function llamarArray() {
+//   const url = "data/empleados.json";
+
+//   fetch(url)
+//     .then((response) => {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then((data) => {
+//       mostrarHTML(data);
+//     });
+// }
+
+// function mostrarHTML(data) {
+//   const container = document.querySelector(".contenido");
+
+//   let html = "";
+
+//   data.forEach((user) => {
+//     const { id, empresa, trabajo, nombre } = user;
+//     html += `
+//    <p>Empleado ${nombre}</p>
+//    <p>Trabajo ${trabajo}</p>
+//    <p>Empresa ${empresa}</p>
+//    `;
+//     container.innerHTML = html;
+//   });
+// }
+
+
+
+
+///Llamado automaticamente al cargar el DOM
+document.addEventListener("DOMContentLoaded", llamarArray);
 
 function llamarArray() {
   const url = "data/empleados.json";
