e41666ea19f222b45c1190b19fc94a2175f76db8
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-02 18:13:40 -0300

    updated fetch to api

--- mid/fetch-API/exercises/weather/js/app.js
+++ mid/fetch-API/exercises/weather/js/app.js
@@ -14,7 +14,10 @@ function buscarClima(e) {
 
   if (cityInput.trim() === "" || selectInput === "") {
     mostrarAlerta("Todos los campos son obligatorios");
+    return;
   }
+
+  consultarAPI(cityInput, selectInput);
 }
 
 function mostrarAlerta(msg) {
@@ -25,14 +28,31 @@ function mostrarAlerta(msg) {
     p = document.createElement("P");
     p.textContent = msg;
     div.classList.add("flex", "justify-center");
-    p.classList.add("alerta","bg-red-400","text-white", "bolder","mt-8","py-4","px-4","max-w-md"
+    p.classList.add(
+      "alerta",
+      "bg-red-400",
+      "text-white",
+      "bolder",
+      "mt-8",
+      "py-4",
+      "px-4",
+      "max-w-md"
     );
-    console.log(p);
     div.appendChild(p);
     container.appendChild(div);
-  }
 
-  setTimeout(() => {
-    p.remove();
-  }, 2500);
+    setTimeout(() => {
+      div.remove();
+    }, 2500);
+  }
+}
+
+//esperando a que se active la apiKey
+function consultarAPI(city, country) {
+  const apiId = '226c26da59da26a9eb31522ae18ae538';
+
+  const url = 
+  `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
+
+  console.log(url);
 }
