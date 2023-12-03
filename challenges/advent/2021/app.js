3f6a4b9f4fef75b0e632d171354eb7e140ca3bc7
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-03 15:48:41 -0300

    add challenges

---
+++ challenges/advent/2021/app.js
@@ -0,0 +1,353 @@
+/*Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color. Haz una función que devuelva una lista con todas las ovejas que sean de color rojo y que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios.
+
+Por ejemplo, si tenemos las ovejas: 
+
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo'},
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo'}
+]
+
+Al ejecutar el método debería devolver lo siguiente:
+
+const ovejasFiltradas = contarOvejas(ovejas)
+
+console.log(ovejasFiltradas)
+
+// [{ name: 'Navidad', color: 'rojo' },
+//  { name: 'Ki Na Ma', color: 'rojo' }]
+
+*/
+
+export default function contarOvejas(ovejas) {
+    // aquí tu magia
+    return ovejas
+  }
+
+
+
+
+  /*Te ha llegado una carta ✉️ con todos los regalos que debes preparar. El tema es que es una cadena de texto y es muy difícil de leer 😱. ¡Menos mal que han puesto cada regalo separado por espacio! (aunque ten cuidado, porque al ser niños, igual han colado más espacios de la cuenta)
+
+Encima nos hemos dado cuenta que algunas palabras vienen con un _ delante de la palabra, por ejemplo _playstation, que significa que está tachado y no se tiene que contar.
+
+Transforma el texto a un objeto que contenga el nombre de cada regalo y las veces que aparece. Por ejemplo, si tenemos el texto: 
+
+
+const carta = 'bici coche balón _playstation bici coche peluche'
+
+
+Al ejecutar el método debería devolver lo siguiente:
+const regalos = listGifts(carta)
+
+console.log(regalos)
+/*
+{
+  bici: 2,
+  coche: 2,
+  balón: 1,
+  peluche: 1
+}
+*/
+
+
+export default function listGifts(letter) {
+    // ¡Tú puedes!
+    return {}
+   }
+
+
+
+
+
+
+   /*El Grinch está abriendo las cartas que iban a Santa Claus y las está dejando hechas un lío. 😱
+
+Las cartas son una cadena de texto que incluyen regalos y paréntesis ().
+
+Para saber si una carta es válida ✅, debes comprobar que los paréntesis cierran correctamente y que, además, no vayan vacíos.
+
+¡Pero ojo! Porque el Grinch ha dejado llaves { y corchetes [ dentro de los paréntesis que hacen que no sean válidas. Por suerte sólo los ha dejado en medio de los paréntesis...
+
+Ejemplos:
+
+
+"bici coche (balón) bici coche peluche" // -> ✅
+"(muñeca) consola bici" // ✅
+
+"bici coche (balón bici coche" // -> ❌
+"peluche (bici [coche) bici coche balón" // -> ❌
+"(peluche {) bici" // -> ❌
+"() bici" // ❌
+
+Crea una función que pasándole el texto de la carta, devuelva true si es válida y false si no lo es. ¡Y acaba con la travesura del Grinch!
+*/
+
+
+export default function isValid(letter) {
+    // ¡No dejes que el Grinch gane!
+    return true
+   }
+
+
+
+
+
+
+
+
+   /*  ¡Es hora de poner el árbol de navidad en casa! 🎄
+
+Para ello vamos a crear una función que recibe la altura del árbol, que será un entero positivo del 1 a, como máximo, 100.
+
+Si le pasamos el argumento 5, se pintaría esto: 
+____*____
+___***___
+__*****__
+_*******_
+*********
+____#____
+____#____
+
+
+
+Creamos un triángulo de asteriscos * con la altura proporcionada y, a los lados, usamos el guión bajo _ para los espacios. Es muy importante que nuestro árbol siempre tenga la misma longitud por cada lado.
+Todos los árboles, por pequeños o grandes que sean, tienen un tronco de dos líneas de #.
+
+Otro ejemplo con un árbol de altura 3:
+__*__
+_***_
+*****
+__#__
+__#__
+
+
+Ten en cuenta que el árbol es un string y necesitas los saltos de línea \n para cada línea para que se forme bien el árbol.
+*/
+
+
+export default function createXmasTree(height) {
+    // ¡Y no olvides también poner los turrones!
+    return ''
+  }
+
+
+
+
+
+
+
+
+
+
+
+  /* Con la emoción, ya estamos empezando a contar los días del calendario hasta el 25 de diciembre 📆.
+
+Para ayudar a esto, vamos a crear una función que pasándole una instancia de Date nos diga el número de días que faltan.
+
+Veamos unos ejemplos:
+const date1 = new Date('Dec 1, 2021')
+daysToXmas(date1) // 24
+const date2 = new Date('Dec 24, 2021 00:00:01')
+daysToXmas(date2) // 1
+const date3 = new Date('Dec 24, 2021 23:59:59')
+daysToXmas(date3) // 1
+const date4 = new Date("December 20, 2021 03:24:00")
+daysToXmas(date4) // 5
+
+
+
+El resultado tiene que ser un número entero y, como ves, aunque falte un segundo hasta el siguiente día, se entiende que todavía falta un día.
+
+¡Pero ojo! También hay que indicar si la fecha es del mismo día (devolveríamos 0) o si es una fecha futura (devolveríamos el número de días en negativo -):
+
+const date = new Date('Dec 25, 2021')
+daysToXmas(date) // 0
+const date1 = new Date('Dec 26, 2021')
+daysToXmas(date1) // -1
+const date2 = new Date('Dec 31, 2021')
+daysToXmas(date2) // -6
+const date3 = new Date('Jan 1, 2022 00:00:01')
+daysToXmas(date3) // -7
+const date4 = new Date('Jan 1, 2022 23:59:59')
+daysToXmas(date4) // -7
+
+Por cierto, la fecha de referencia para saber si es 25 de diciembre es Dec 25, 2021.
+*/
+
+
+export default function daysToXmas(date) {
+    // ¡Y no olvides compartir tu solución en redes!
+    return 0
+  }
+
+
+
+  /* Antes de poder disfrutar de la navidad... nos toca terminar de rematar los exámenes finales. ¡Y toca un poco de matemáticas! 😱
+
+A una función se le pasan dos parámetros: un Array con números y el resultado que se espera.
+
+La función debe devolver los dos valores del Array que sumen el resultado esperado. Como a veces pueden haber más de dos valores que sumen, se devolverá el primero empezando por la izquierda que encuentre otro par, sin importar lo lejos que esté a la derecha.
+
+Si no se encuentra, se devuelve null.
+
+Veamos unos ejemplos:   
+
+sumPairs([3, 5, 7, 2], 10) // [3, 7]
+sumPairs([-3, -2, 7, -5], 10) // null
+sumPairs([2, 2, 3, 1], 4) // [2, 2]
+sumPairs([6, 7, 1, 2], 8) // [6, 2]
+sumPairs([0, 2, 2, 3, -1, 1, 5], 6) // [1, 5]
+
+
+El resultado tiene que ser un array con dos números.
+
+Una vez que tengas el resultado... ¿cómo podrías hacer que fuese lo más óptimo posible para no tener que recorrer las mismas situaciones dos veces 🤔?
+
+
+
+*/
+
+export default function sumPairs(numbers, result) {
+    // ¡Y no olvides compartir tu solución en redes!
+    return null
+  }
+
+
+
+
+
+  /* Mi amigo Dani está trabajando en una tienda y con la llegada de las navidades tiene el almacén hecho un desastre y no encuentra nada.
+
+Vamos a crear una función contains que recibe dos parámetros: un objeto que define el almacén y el producto que buscamos.
+
+La función debe devolver un booleano que indique si se encuentra el string como valor en algún nivel del objeto. Veamos unos ejemplos:
+
+
+const almacen = {
+  'estanteria1': {
+    'cajon1': {
+      'producto1': 'coca-cola',
+      'producto2': 'fanta',
+      'producto3': 'sprite'
+    }
+  },
+  'estanteria2': {
+    'cajon1': 'vacio',
+    'cajon2': {
+      'producto1': 'pantalones',
+      'producto2': 'camiseta' // <- ¡Está aquí!
+    }
+  }
+}
+            
+contains(almacen, 'camiseta') // true
+
+const otroAlmacen = {
+  'baul': {
+    'fondo': {
+      'objeto': 'cd-rom',
+      'otro-objeto': 'disquette',
+      'otra-cosa': 'mando'
+    }
+  }
+}
+  
+contains(otroAlmacen, 'gameboy') // false
+
+
+Ten en cuenta que la tienda es enorme. Tiene diferentes almacenes y, como has visto en los ejemplos, cada uno puede tener diferentes organizaciones.Lo importante es buscar que el producto está en los almacenes.
+
+
+*/
+
+
+export default function contains(store, product) {
+    // ¡Y no olvides compartir tu solución en redes!
+    return false
+  }
+
+
+
+
+//   Invertir en criptomonedas es casi un deporte de riesgo. El otro día hackearon Bitmart y ha hecho que el valor de Bitcoin, y otras monedas, bajase un 25%.
+
+// Vamos a escribir una función que reciba la lista de precios de una criptomoneda en un día y debemos devolver la ganancia máxima que podríamos sacar si compramos y vendemos la inversión el mismo día.
+
+// La lista de precios es un array de números y representa el tiempo de izquierda a derecha. Por lo que ten en cuenta que no puedes comprar a un precio que esté a la derecha de la venta y no puedes vender a un precio que esté a la izquierda de la compra.
+
+// Por ejemplo:
+
+
+/* 
+const pricesBtc = [39, 18, 29, 25, 34, 32, 5]
+maxProfit(pricesBtc) // -> 16 (compra a 18, vende a 34)
+
+const pricesEth = [10, 20, 30, 40, 50, 60, 70]  
+maxProfit(pricesEth) // -> 60 (compra a 10, vende a 70)
+    
+
+Si ese día no se puede sacar ningún beneficio, tenemos que devolver -1 para evitar que hagamos una locura:
+
+
+devolver -1 para evitar que hagamos una locura:
+
+const pricesDoge = [18, 15, 12, 11, 9, 7]
+maxProfit(pricesDoge) = // -> -1 (no hay ganancia posible)
+
+const pricesAda = [3, 3, 3, 3, 3]
+maxProfit(pricesAda) = // -> -1 (no hay ganancia posible)
+
+*/
+
+export default function maxProfit(prices) {
+    // ¡Y no olvides compartir tu solución en redes!
+    return false
+  }
+
+
+
+
+
+
+
+
+  /* 
+  En la fábrica de Papa Noél 🎅 se acerca el día especial... y todavía tenemos un montón de cosas por contar. 😅
+
+Por suerte a Mark Zucktheelf 🧝 se le ha ocurrido crear una función que permita agrupar un array, que puede ser de valores u objetos, a través de una función o de una propiedad.
+
+Nos trae un montón de ejemplos:
+  
+
+groupBy([6.1, 4.2, 6.3], Math.floor) // { 6: [6.1, 6.3], 4: [4.2] }
+groupBy(['one', 'two', 'three'], 'length') // { 3: ['one', 'two'], 5: ['three'] }
+groupBy([{age: 23}, {age: 24}], 'age') // { 23: [{age: 23}], 24: [{age: 24}] }
+
+groupBy(
+  [1397639141184, 1363223700000],
+  timestamp => new Date(timestamp).getFullYear()
+)
+// { 2013: [1363223700000], 2014: [1397639141184] }
+
+groupBy([
+  { title: 'JavaScript: The Good Parts', rating: 8 },
+  { title: 'Aprendiendo Git', rating: 10 },
+  { title: 'Clean Code', rating: 9 },
+], 'rating')
+// { 8: [{ title: 'JavaScript: The Good Parts', rating: 8 }],
+//   9: [{ title: 'Clean Code', rating: 9 }],
+//   10: [{ title: 'Aprendiendo Git', rating: 10 }] }
+
+Como ves, la función groupBy recibe una colección (array) y una función o una propiedad, y devuelve un objeto con claves que son los valores de la función ejecutada pasando como argumento cada elemento o de la propiedad por cada elemento. Luego los valores son un array de los valores que tengan la misma llave.
+
+La dificultad del reto está más en comprender la función que en la implementación. ¡Suerte!.
+  */
+
+export default function groupBy(collection, it) {
+    // ¡No olvides compartir tu solución en redes!
+    return {}
+  }
---
+++ challenges/advent/2023/app.js
@@ -0,0 +1,112 @@
+//ADVENTJS 2023
+// https://adventjs.dev/es
+
+/*
+
+1)
+
+En la fábrica de juguetes del Polo Norte, cada juguete tiene un número de identificación único.
+
+Sin embargo, debido a un error en la máquina de juguetes, algunos números se han asignado a más de un juguete.
+
+¡Encuentra el primer número de identificación que se ha repetido, donde la segunda ocurrencia tenga el índice más pequeño!
+
+En otras palabras, si hay más de un número repetido, debes devolver el número cuya segunda ocurrencia aparezca primero en la lista. Si no hay números repetidos, devuelve -1.
+
+
+
+
+const giftIds = [2, 1, 3, 5, 3, 2]
+const firstRepeatedId = findFirstRepeated(giftIds)
+console.log(firstRepeatedId) // 3
+// Aunque el 2 y el 3 se repiten
+// el 3 aparece primero por segunda vez
+
+const giftIds2 = [1, 2, 3, 4]
+const firstRepeatedId2 = findFirstRepeated(giftIds2)
+console.log(firstRepeatedId2) // -1
+// Es -1 ya que no se repite ningún número
+
+const giftIds3 = [5, 1, 5, 1]
+const firstRepeatedId3 = findFirstRepeated(giftIds3)
+console.log(firstRepeatedId3) // 5
+
+*/
+
+function findFirstRepeated(gifts) {
+  // Code here
+  return 0;
+}
+
+
+
+
+
+/*
+  2)
+  En el taller de Santa, los elfos tienen una lista de regalos que desean fabricar y un conjunto limitado de materiales.
+
+Los regalos son cadenas de texto y los materiales son caracteres. Tu tarea es escribir una función que, dada una lista de regalos y los materiales disponibles, devuelva una lista de los regalos que se pueden fabricar.
+
+Un regalo se puede fabricar si contamos con todos los materiales necesarios para fabricarlo. 
+
+
+
+const gifts = ['tren', 'oso', 'pelota']
+const materials = 'tronesa'
+
+manufacture(gifts, materials) // ["tren", "oso"]
+// 'tren' SÍ porque sus letras están en 'tronesa'
+// 'oso' SÍ porque sus letras están en 'tronesa'
+// 'pelota' NO porque sus letras NO están en 'tronesa'
+
+const gifts = ['juego', 'puzzle']
+const materials = 'jlepuz'
+
+manufacture(gifts, materials) // ["puzzle"]
+
+const gifts = ['libro', 'ps5']
+const materials = 'psli'
+
+manufacture(gifts, materials) // []*/
+
+function manufacture(gifts, materials) {
+  // Code here
+  return [];
+}
+
+
+
+
+/* En el taller de Santa, un elfo travieso ha estado jugando en la cadena de fabricación de regalos, añadiendo o eliminando un paso no planificado.
+
+Tienes la secuencia original de pasos en la fabricación original y la secuencia modificada modified que puede incluir un paso extra o faltar un paso.
+
+Tu tarea es escribir una función que identifique y devuelva el primer paso extra que se ha añadido o eliminado en la cadena de fabricación. Si no hay ninguna diferencia entre las secuencias, devuelve una cadena vacía.
+
+const original = 'abcd'
+const modified = 'abcde'
+findNaughtyStep(original, modified) // 'e'
+
+const original = 'stepfor'
+const modified = 'stepor'
+findNaughtyStep(original, modified) // 'f'
+
+const original = 'abcde'
+const modified = 'abcde'
+findNaughtyStep(original, modified) // ''
+
+
+
+A tener en cuenta:
+
+Siempre habrá un paso de diferencia o ninguno.
+La modificación puede ocurrir en cualquier lugar de la cadena.
+La secuencia original puede estar vacía
+
+*/
+
+function findNaughtyStep(original, modified) {
+  // Code here
+  return "";
+}
