40b6efb9980e617c5d81ea443bdde83aef071014
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-04 14:33:01 -0300

    get dishes for category

--- mid/fetch-API/exercises/recetas/js/app.js
+++ mid/fetch-API/exercises/recetas/js/app.js
@@ -4,7 +4,14 @@ const resultCategories = document.querySelector("#resultado");
 
 document.addEventListener("DOMContentLoaded", () => {
   traerCategorias();
+
+  selectCategories.addEventListener("change", seleccionarCategorias);
 });
+
+const objCategory = {
+  nombre: "",
+  id: "",
+};
 
 function traerCategorias(e) {
   const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
@@ -22,12 +29,25 @@ function infoCategorias(info) {
   info.forEach((cat) => {
     const { idCategory, strCategory } = cat;
     const option = document.createElement("OPTION");
-    option.value = idCategory;
+    option.value = strCategory;
     option.textContent = strCategory;
 
     selectCategories.appendChild(option);
   });
-
-  console.log(selectCategories.value);
 }
 
+function seleccionarCategorias(e) {
+
+  const selectedCategory = e.target.value;
+
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedCategory}`;
+  // console.log(url);
+  fetch(url)
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
