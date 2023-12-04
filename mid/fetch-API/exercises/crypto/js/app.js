dd5c627d05cea67b4947cf6b2bb19feee9963317
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-04 09:51:19 -0300

    add validation of the object with the data from the api

--- mid/fetch-API/exercises/crypto/js/app.js
+++ mid/fetch-API/exercises/crypto/js/app.js
@@ -1,8 +1,56 @@
 const select = document.querySelector("#criptomonedas");
+const moneda = document.querySelector("#moneda");
+const form = document.querySelector("#formulario");
+const result = document.querySelector("#resultado");
 
 document.addEventListener("DOMContentLoaded", () => {
   consultarCriptomonedas();
+
+  select.addEventListener("change", cotizarCriptomonedas);
+  moneda.addEventListener("change", cotizarMoneda);
+
+  form.addEventListener("submit", validarObj);
 });
+
+objCotizar = {
+  moneda: "",
+  criptomoneda: "",
+};
+
+function cotizarMoneda(e) {
+  objCotizar.moneda = e.target.value;
+}
+
+function cotizarCriptomonedas(e) {
+  objCotizar.criptomoneda = e.target.value;
+  console.log(objCotizar);
+}
+
+function validarObj(e) {
+  e.preventDefault();
+  const { moneda, criptomoneda } = objCotizar;
+  if (moneda === "" || criptomoneda === "") {
+    return mostrarAlerta("Campos vacíos");
+  }
+}
+
+function mostrarAlerta(msg) {
+  const alerta = document.querySelector(".alert");
+
+  if (!alerta) {
+    const div = document.createElement("DIV");
+    const p = document.createElement("P");
+    p.textContent = msg;
+    div.classList.add("alert");
+
+    div.appendChild(p);
+    result.appendChild(div);
+
+    setTimeout(() => {
+      div.remove();
+    }, 2000);
+  }
+}
 
 function obtenerCriptomonedas(criptomonedas) {
   return new Promise((resolve) => {
