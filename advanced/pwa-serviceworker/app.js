faa25f7c239b52750dcc3be2319112661dcc51d3
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-09 16:49:28 -0300

    add initial commit for pwa

---
+++ advanced/pwa-serviceworker/app.js
@@ -0,0 +1,10 @@
+/*
+Progressive web apps (PWA) y service workers
+
+PWA: rapidas, instalables y soporte offline.
+
+Service Workers: es la base de una PWA, son scripts que están corriendo todo el tiempo, funcionan offline, no tiene acceso al DOM, carga de forma instantanea, sincronizacion de datos.
+se utiliza self en cambio de window
+se utiliza caches en cambio de document
+se utiliza fetch en cambio de localStorage
+*/
