f163dd7cd8b06ba0f070985a814034ad1ba7742d
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-13 18:48:53 -0300

    add nodemon

--- projects/fullstack-js/backend/index.js
+++ projects/fullstack-js/backend/index.js
@@ -2,9 +2,9 @@ import express from "express";
 
 const app = express();
 
-app.use("/", (req, res)=> {
-    res.send("Hola")
-})
+app.use("/", (req, res) => {
+  res.send("Hola");
+});
 
 app.listen(4000, () => {
   console.log("servidor funcionando en el puerto 4000");
