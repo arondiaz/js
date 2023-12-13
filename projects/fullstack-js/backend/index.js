42ce52595d777f73fa75dc43fde5941e50ab1fe1
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-13 18:44:55 -0300

    add initial commit of project and configure backend

---
+++ projects/fullstack-js/backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use("/", (req, res)=> {
+    res.send("Hola")
+})
+
+app.listen(4000, () => {
+  console.log("servidor funcionando en el puerto 4000");
+});
