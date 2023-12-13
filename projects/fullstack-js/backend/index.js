484cd6561e724c20c7700a9ebbe3dab284d8bf15
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-13 19:20:31 -0300

    configure db connection

--- projects/fullstack-js/backend/index.js
+++ projects/fullstack-js/backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
+dotenv.config();
+connectDB();
 
 app.use("/", (req, res) => {
   res.send("Hola");
 });
 
-app.listen(4000, () => {
-  console.log("servidor funcionando en el puerto 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`servidor funcionando en el puerto ${PORT}`);
 });
