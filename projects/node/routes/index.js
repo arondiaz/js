56e174311561bb9c4011811b7fa65c9e240e3cb4
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-30 16:05:07 -0300

    add master page with pug

--- projects/node/routes/index.js
+++ projects/node/routes/index.js
@@ -5,11 +5,17 @@ const router = express.Router();
 // request => lo que envio
 // response => lo que recibo
 router.get("/", (req, res) => {
-  res.send("Inicio");
+  res.render("inicio");
 });
 
 // render muestra una vista
 router.get("/nosotros", (req, res) => {
+  // const viajes = "Viaje a Alemania";
+
+  //   res.render("nosotros", {
+  //     textoViajes : viajes
+  //   });
+
   res.render("nosotros");
 });
 
