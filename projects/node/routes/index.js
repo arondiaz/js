8d4c2e7f831701502962abefef484e64529dc647
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-02 10:30:46 -0300

    add dynamic routing and view

--- projects/node/routes/index.js
+++ projects/node/routes/index.js
@@ -5,7 +5,9 @@ const router = express.Router();
 // request => lo que envio
 // response => lo que recibo
 router.get("/", (req, res) => {
-  res.render("inicio");
+  res.render("inicio", {
+    pagina: "Inicio",
+  });
 });
 
 // render muestra una vista
@@ -16,7 +18,21 @@ router.get("/nosotros", (req, res) => {
   //     textoViajes : viajes
   //   });
 
-  res.render("nosotros");
+  res.render("nosotros", {
+    pagina: "Nosotros",
+  });
+});
+
+router.get("/viajes", (req, res) => {
+  res.render("viajes", {
+    pagina: "Viajes",
+  });
+});
+
+router.get("/testimonios", (req, res) => {
+  res.render("testimonios", {
+    pagina: "Testimonios",
+  });
 });
 
 export default router;
