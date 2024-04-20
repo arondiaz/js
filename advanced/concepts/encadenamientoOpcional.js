78aab22328d883182bb72e2bc627e4132f18513e
Arón Díaz <arondiaz.cpn@gmail.com>
2024-04-20 16:13:21 -0300

    update optional chaining

--- advanced/concepts/encadenamientoOpcional.js
+++ advanced/concepts/encadenamientoOpcional.js
@@ -1,4 +1,5 @@
 //Encadenamiento opcional
+// https://www.youtube.com/watch?v=ClUOu_npNWM
 // https://www.youtube.com/watch?v=sNlY4B6VgZE
 
 const usuario = {
@@ -16,8 +17,11 @@ const usuario = {
 //Error
 // console.log(usuario.rol.backend);
 
-//Undefined
+//Undefined - Funciona para propiedades anidadas, ya que usuario.rol devuelve undefined y usuario.rol.backend devuelve ERROR, en este caso si es necesario usar Optional Chaining (?);
 console.log(usuario.rol?.backend);
+
+//Undefined
+console.log(usuario.rol);
 
 //Anterior al 2020 se hacia de esta forma la comprobación
 const resultado = usuario.rol ? usuario.rol.backend : undefined;
