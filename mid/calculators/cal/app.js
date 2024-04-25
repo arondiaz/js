f3c8892c784afb19f791f6eb0d38243f195edfaa
Arón Díaz <arondiaz.cpn@gmail.com>
2024-04-25 19:02:00 -0300

    add calculator

--- mid/calculators/cal/app.js
+++ mid/calculators/cal/app.js
@@ -1,13 +1,23 @@
-const operacion = (value) =>(document.querySelector("#display").value += value);
-
-const igual = () => {
-  const d = document.querySelector("#display").value;
-  if (d.trim() === "") {
-    return;
-  }
-  const r = eval(d);
-
-  document.querySelector("#display").value = r;
+const add = (val) => {
+  let screen = document.querySelector("#screen");
+  screen.value += val;
 };
 
-const borrar = () => (document.querySelector("#display").value = "");
+const clean = () => {
+  let screen = document.querySelector("#screen");
+
+  screen.value = "";
+};
+
+const operation = () => {
+  let screen = document.querySelector("#screen");
+
+  screen.value = eval(screen.value);
+};
+
+const suprimir = () => {
+  let screen = document.querySelector("#screen").value;
+
+  screen = screen.slice(0, -1);
+  document.querySelector("#screen").value = screen;
+};
