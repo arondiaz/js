5b4cf550b1b9b3a37bc9221ee8b122c5c985b444
Arón Díaz <arondiaz.cpn@gmail.com>
2024-04-29 20:03:42 -0300

    resolve exercises

--- advanced/concepts/mircha.js
+++ advanced/concepts/mircha.js
@@ -299,3 +299,55 @@ const discount = (num, descuento) => {
 discount(69, 0);
 
 // 17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
+
+calculateYears = (fecha = undefined) => {
+  if (fecha === undefined) return console.log("Ingresa una fecha");
+
+  if (!(fecha instanceof Date)) return console.log("Ingrsa una fecha válida");
+
+  let hoyMenosFecha = new Date().getTime() - fecha.getTime();
+  let yearsEnMS = 1000 * 60 * 60 * 24 * 365;
+  let yearHumanos = Math.floor(hoyMenosFecha / yearsEnMS);
+
+  return Math.sign(yearHumanos) === -1
+    ? console.log(
+        `Faltan ${Math.abs(yearHumanos)} años para el ${fecha.getFullYear()}`
+      )
+    : Math.sign(yearHumanos) === 1
+    ? console.log(`Han pasado ${yearHumanos} años desde ${fecha.getFullYear()}`)
+    : console.log(`Estamos en el año actual ${fecha.getFullYear()}`);
+
+  console.log(hoyMenosFecha);
+  console.log(yearsEnMS);
+  console.log(yearHumanos);
+};
+
+calculateYears(new Date(2024, 4, 23));
+
+// 18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
+
+const countVocalsAndConsonants = (palabra) => {
+  if (typeof palabra !== "string") {
+    return console.log("debes ingresar un string");
+  }
+
+  let vocales = 0;
+  let consonante = 0;
+  let word = palabra.toLowerCase();
+
+  for (const i of word) {
+    if (/[aeiou]/.test(i)) vocales++;
+
+    if (/[qwrtypñlkjhgfdszxcvbnm]/.test(i)) consonante++;
+  }
+
+  return console.log({
+    palabra,
+    vocales,
+    consonante,
+  });
+};
+countVocalsAndConsonants("Hola Mundo");
+
+// 19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
+// 20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
