1f4e481296de9c53665811ca0ad5e0c4b5503882
Arón Díaz <arondiaz.cpn@gmail.com>
2024-04-30 18:11:08 -0300

    resolve exercise of arrays

--- advanced/concepts/mircha.js
+++ advanced/concepts/mircha.js
@@ -371,3 +371,56 @@ const validateEmail = (email) => {
 };
 
 validateEmail("pepe@mail.com");
+
+// 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+
+const alCuadrado = (array = undefined) => {
+  if (array === undefined) return console.log("No ingresaste números");
+
+  if (!(array instanceof Array))
+    return console.log("El valor que ingresaste no es un arreglo");
+
+  if (array.length === 0) return console.log("El array está vacío");
+
+  let result = [];
+  for (const iterator of array) {
+    if (typeof iterator !== "number") {
+      return console.log("Debe ingresar números");
+    }
+    result.push(iterator * iterator);
+  }
+
+  return console.log(result);
+};
+
+alCuadrado([1, 2, 3]);
+
+// 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+
+const maxMin = (array) => {
+  let maxAndMin = [];
+
+  maxAndMin.push(Math.min(...array));
+  maxAndMin.push(Math.max(...array));
+
+  return console.log(maxAndMin);
+};
+
+maxMin([1, 4, -5, 798]);
+
+// 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
+
+const parImparArr = (array) => {
+  let pares = [];
+  let impares = [];
+  for (const i of array) {
+    if (i % 2 === 0) {
+      pares.push(i);
+    } else {
+      impares.push(i);
+    }
+  }
+  return { pares, impares };
+};
+
+console.log(parImparArr([1, 2, 3, 4, 5, 6, 7, 8, 9]));
