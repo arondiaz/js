3bfebca30ff9946755ec4d2ac3fe2cfeae691a58
Arón Díaz <arondiaz.cpn@gmail.com>
2024-05-02 17:02:13 -0300

    add initial deck

---
+++ games/blackjack/app.js
@@ -0,0 +1,27 @@
+/*
+2C = Clubs
+2D = Diamons
+2H = Hearts
+2S = Spades
+*/
+
+let deck = [];
+const types = ["C", "D", "H", "S"];
+const especiales = ["A", "J", "K", "Q"];
+
+function crearDeck() {
+  for (let i = 2; i <= 10; i++) {
+    for (let tipo of types) {
+      deck.push(i + tipo);
+    }
+  }
+
+  for (let tipo of types) {
+    for (let especial of especiales) {
+      deck.push(especial + tipo);
+    }
+  }
+  console.log(deck);
+}
+
+crearDeck();
