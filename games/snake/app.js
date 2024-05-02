fb1cd2ca035730ccdda49fa9a682e14959f8b71a
Arón Díaz <arondiaz.cpn@gmail.com>
2024-05-02 09:42:55 -0300

    add function for create board, draw squares, draw snake and update score

--- games/snake/app.js
+++ games/snake/app.js
@@ -29,6 +29,13 @@ startButton.addEventListener("click", startGame);
 
 function startGame() {
   setGame();
+
+  gameOverSign.style.display = "none";
+  startButton.disabled = true;
+  drawSnake();
+
+  updateScore();
+  createRandomFood();
 }
 
 const setGame = () => {
@@ -45,5 +52,43 @@ const setGame = () => {
 };
 
 function createBoard() {
-    
+  boardSquares.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      const squareValue = `${rowIndex}${columnIndex}`;
+      const squareElement = document.createElement("DIV");
+      squareElement.setAttribute("class", "square emptySquare");
+      squareElement.setAttribute("id", squareValue);
+      board.appendChild(squareElement);
+      emptySquare.push(squareValue);
+    });
+  });
+}
+
+function drawSquare(square, type) {
+  const [row, column] = square.split("");
+  boardSquares[row][column] = squareTypes[type];
+  const squareElement = document.getElementById(square);
+  squareElement.setAttribute("class", `square ${type}`);
+
+  if (type === "emptySquare") {
+    emptySquare.push(square);
+  } else {
+    if (emptySquare.indexOf(square) !== -1) {
+      emptySquare.splice(emptySquare.indexOf(square), 1);
+    }
+  }
+}
+
+function drawSnake() {
+  snake.forEach((square) => drawSquare(square, "snakeSquare"));
+}
+
+function updateScore() {
+  scoreBoard.innerText = score;
+}
+
+function createRandomFood() {
+  const randomEmptySquare =
+    emptySquare[Math.floor(Math.random() * emptySquare.length)];
+  drawSquare(randomEmptySquare, "foodSquare");
 }
