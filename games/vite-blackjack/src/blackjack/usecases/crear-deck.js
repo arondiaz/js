5d1010ddd066af0c358f899b2a937b88f8673f75
Arón Díaz <arondiaz.cpn@gmail.com>
2024-06-03 19:46:27 -0300

    add validation and documentation

--- games/vite-blackjack/src/blackjack/usecases/crear-deck.js
+++ games/vite-blackjack/src/blackjack/usecases/crear-deck.js
@@ -1,6 +1,18 @@
 import _ from "underscore";
 
+/**
+ *
+ * @param {Array<String>} tiposDeCartas ejemplo ["C", "D", "H", "S"]
+ * @param {Array<String>} tiposEspeciales ejemplo  ["A", "J", "K", "Q"]
+ * @returns {Array} retorna un nuevo deck
+ */
 export const crearDeck = (tiposDeCartas, tiposEspeciales) => {
+  if (!tiposDeCartas || tiposDeCartas.length === 0)
+    throw new Error("tiposDeCartas es obligatorio como un array de string");
+
+  if (!tiposEspeciales || tiposEspeciales.length === 0)
+    throw new Error("tiposEspeciales es obligatorio como un array de string");
+  
   let deck = [];
 
   for (let i = 2; i <= 10; i++) {
@@ -16,4 +28,4 @@ export const crearDeck = (tiposDeCartas, tiposEspeciales) => {
   }
 
   return _.shuffle(deck);
-}
+};
