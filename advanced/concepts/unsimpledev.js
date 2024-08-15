06d9d9f2d7f6c3b5688c0d28719a005a9d3f50f7
Arón Díaz <arondiaz.cpn@gmail.com>
2024-08-15 18:55:56 -0300

    review exercises

---
+++ advanced/concepts/unsimpledev.js
@@ -0,0 +1,29 @@
+// 1 Puntuación de un equipo de fútbol - LA FAROLERA FC desea obtener la cantidad de puntos que hizo en este torneo.
+// Escribir un algoritmo que permita ingresar la cantidad de partidos ganados, la cantidad de partidos empatados y la cantidad de partidos perdidos, y en base a estos datos calcule y muestre la cantidad de puntos. Se debe tener en cuenta lo siguiente:
+// – Cada partido ganado suma 3 puntos
+// – Cada partido empatado suma 1 punto
+// – Cada partido perdido suma 0 puntos
+
+// function points() {
+//     let win = Number(prompt("Partidos ganados?"))
+//     let lose = Number(prompt("Partidos perdidos?"))
+//     let same = Number(prompt("Partidos empatados?"))
+//     let result = 0;
+
+//     result += win * 3
+//     result += same;
+//     return console.log(result);
+
+// }
+
+// points();
+
+
+
+
+// 2️ Conversión de millas marinas a metros - Transforma distancias marinas en su equivalente métrico.
+// Diseñar el algoritmo que lea el valor correspondiente a una distancia en millas marinas y las escriba expresadas en metros. Sabiendo que 1 milla marina equivale a 1852 metros.
+
+// 3️ Cálculo de descuentos en compras -  Diseñar el algoritmo que muestre el porcentaje descontado en una compra, se ingresa por teclado el precio original y el precio pagado.
+
+// 4️ Cálculo de remuneración-  Diseñar el algoritmo que indique cuánto debe cobrar un empleado según su tarifa por hora y sus horas trabajadas.
